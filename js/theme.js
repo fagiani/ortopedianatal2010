a3bbe2fbe19c285b6dbbbc28b20a0fc1d8d7378f
tink <tink@imac.local.tink.com.br>
2010-01-20 18:39:09 -0300

    Putting the right date to js counter

--- js/theme.js
+++ js/theme.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($){
   $("a[href^='http:']").not("[href*='localhost']").attr('target','_blank');
-  var difference = (new Date("10/20/2010") - new Date()) / 1000;
+  var difference = (new Date("04/22/2010") - new Date()) / 1000;
   $("#days_remain").html(Math.floor(( difference < 0 ? 0 : difference ) / (60 * 60 * 24)) + ' Dias');
 });
