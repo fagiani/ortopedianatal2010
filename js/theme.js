bf1ac7a0cc200f7283bb45434312548cc869f719
Joice Taciana <joicetaciana@gmail.com>
2010-03-09 12:39:03 -0300

    change the structure of javascript - it doesn't need jquery to run

--- js/theme.js
+++ js/theme.js
@@ -1,5 +1,2 @@
-jQuery(document).ready(function($){
-  $("a[href^='http:']").not("[href*='localhost']").attr('target','_blank');
-  var difference = (new Date("04/22/2010") - new Date()) / 1000;
-  $("#countdown").html(Math.floor(( difference < 0 ? 0 : difference ) / (60 * 60 * 24)) + ' Dias');
-});
+var difference = (new Date("04/22/2010") - new Date()) / 1000;
+document.getElementById("countdown").innerHTML = Math.floor(( difference < 0 ? 0 : difference ) / (60 * 60 * 24)) + ' Dias';
